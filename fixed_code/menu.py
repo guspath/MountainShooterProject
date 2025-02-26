38d98105c9da824f6edf1f48376eb027ce3e88ad
Gustavo Follador <gustavofollador96@email.com>
2025-02-26 13:14:24 -0300

    Criei o diretório assets, adicionando as imagens necessárias para fazer o background do jogo, bem como estruturei o menu inicial e música de fundo. Usei de constantes para poder importa-las ao main e não precisar ficar criando toda hora.

--- fixed_code/menu.py
+++ fixed_code/menu.py
@@ -1,9 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from tkinter.font import Font
+
+import pygame.image
+import pygame.font
+from pygame import Surface
+
+from const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('assets/PRE_ORIG_SIZE.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
 
     def run(self, ):
-        pass
+        pygame.mixer.music.load('./assets/destination-01.mp3')
+        pygame.mixer.music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+            pygame.display.flip()
+
+            #Check for all events
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Window
+                    quit()  # end pygame
+
+
+    def menu_text(self, text_size: int, text: str, text_color :tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont("Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect = Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
