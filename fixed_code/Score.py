9076ebdd22207850714a133ac5cb8a39f5b126a9
Gustavo Follador <gustavofollador96@email.com>
2025-03-20 12:23:22 -0300

    Finished score feature implementation with Dbase in Sqlite3 using proxy design pattern

--- fixed_code/Score.py
+++ fixed_code/Score.py
@@ -1,7 +1,8 @@
 import sys
-import datetime
+from datetime import datetime
+
 import pygame
-from pygame import Surface, K_RETURN, K_BACKSPACE
+from pygame import Surface, K_RETURN, K_BACKSPACE, K_ESCAPE, KEYDOWN
 from pygame.font import Font
 
 from fixed_code.DBProxy import DBProxy
@@ -47,6 +48,8 @@ class Score:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == K_RETURN and len(name) == 4:
                         db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
+                        self.show()
+                        return
                     elif event.key == K_BACKSPACE:
                         name = name[:-1]
                     else:
@@ -61,9 +64,27 @@ class Score:
         pygame.mixer.music.load('./assets/Score.mp3')
         pygame.mixer.music.play(-1)
         self.window.blit(source=self.surf, dest=self.rect)
+        self.score_text(48, 'TOP 10 SCORE', C_YELLOW, SCORE_POS['Title'])
+        self.score_text(20, 'NAME       SCORE            DATE         ', C_YELLOW, SCORE_POS['Label'])
+        db_proxy = DBProxy('DBScore')
+        list_score = db_proxy.retrieve_top10()
+        db_proxy.close()
+
+        for player_score in list_score:
+            id_, name, score, date = player_score
+            self.score_text(20, f'{name}      {score:05d}       {date}', C_YELLOW,
+                                    SCORE_POS[list_score.index(player_score)])
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        return
+
             pygame.display.flip()
-            pass
+
 
     def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont("Lucida Sans Typewriter", size=text_size)
@@ -75,4 +96,4 @@ def get_formatted_date():
     current_datetime = datetime.now()
     current_time = current_datetime.strftime("%H:%M:%S")
     current_date = current_datetime.strftime("%d/%m/%Y")
-    return f"{current_time} {current_date}"
+    return f"{current_time} - {current_date}"
