372ccb2988aac6a9e40af78808e47fe2330aa204
Gustavo Follador <gustavofollador96@email.com>
2025-03-21 13:16:02 -0300

    Updated all files and generated executable. Project finished

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from cx_Freeze import setup, Executable
-import os
-path = "./assets/"
-assets_list = os.listdir(path)
-assets_list_completa = [os.path.join(path, assets).replace(__old="//", __new="/") for assets in assets_list]
-print(assets_list_completa)
 
 executables = [Executable("main.py")]
-files = {"include_files": assets_list_completa, "packages": ["pygame"]}
 
 setup(
     name="Mountain Project",
